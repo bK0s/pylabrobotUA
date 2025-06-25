db4e9bad37dc2011c5d0eddbbbd613df924e788a
bK0s <bdkosy@gmail.com>
2025-06-25 10:50:33 -0600

    Added initial protocol setup

---
+++ protocols/test.py
@@ -0,0 +1,26 @@
+# import asyncio
+
+# from pylabrobot.liquid_handling import LiquidHandler
+# from pylabrobot.liquid_handling.backends import LiquidHandlerChatterboxBackend
+# from pylabrobot.visualizer.visualizer import Visualizer
+
+# from pylabrobot.resources.tecan import EVO200Deck
+
+# async def setup_():
+#     lh = LiquidHandler(backend=LiquidHandlerChatterboxBackend, deck=EVO200Deck())
+#     await lh.setup()
+#     vis = Visualizer(resource=lh)
+#     await vis.setup()
+      
+
+# async def main():
+# 	await asyncio.get_event_loop(setup_())
+	
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+def function():
+    print("name")
+
+# if __name__ == "__main__":
+#     function()
