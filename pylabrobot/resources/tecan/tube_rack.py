f74aa4dfaed7a79052a18923fb4dc8bb0702a524
bK0s <bdkosy@gmail.com>
2025-08-22 10:20:09 -0600

    created tube rack

--- pylabrobot/resources/tecan/tube_rack.py
+++ pylabrobot/resources/tecan/tube_rack.py
@@ -12,36 +12,32 @@ class TecanTubeRack(TubeRack, TecanResource):
     def __init__(self, name: str, size_x: float, size_y: float, size_z: float, ordered_items: Dict[str, Tube] | None = None, model: str | None = None):
         super().__init__(name, size_x, size_y, size_z, ordered_items, model)
 
-# class TubeSpot(Resource):
-#     # def __init__(self, name: str, size_x=9, size_y=9, size_z=45):
-#     #     self.name = name
-#     #     self.size_x = size_x
-#     #     self.size_y = size_y
-#     #     self.size_z = size_z
-#         def __init__(self, name: str, size_x: float, size_y: float, size_z: float, rotation: Rotation | None = None, category: str | None = None, model: str | None = None, barcode: Barcode | None = None):
-#             super().__init__(name, size_x, size_y, size_z, rotation, category, model, barcode)
-
-#             self.parent: Optional["TecanTubeRack"] = None
-
 def tecan_tube_rack(name: str) -> TubeRack:
     return TecanTubeRack(
-        size_x=40,
-        size_y=200,
-        size_z=3,
-        name='tubeRack',
+        size_x=23,
+        size_y=307,
+        size_z=50,
+        name=name,
         ordered_items=create_ordered_items_2d(
-            test_tube, 1,16,10,10,2,3,10
+            klass=test_tube,
+            num_items_x=1,
+            num_items_y=16,
+            dx=4,
+            dy=1.375,
+            dz=50,
+            item_dx=0,
+            item_dy=18.75
         ),
         model='Whoknows'
     )
 def test_tube(name:str) -> Tube:
-    diameter = 10
+    diameter = 16.5
     return Tube(
     name=name,
     size_x=diameter,
     size_y=diameter,
     size_z=115,
-    model="Falcon 50mL",
-    max_volume=50_000,
+    model="Unknown 10mL",
+    max_volume=10_000,
     material_z_thickness=1.2,
   )
